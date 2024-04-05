9fb2d20a7880a32cfb2aba2a2da00daf6960209f
David Antonio Ramirez Mendez <davidrap087@gmail.com>
2024-04-05 18:28:46 -0400

    Reto Finalizado

--- Projects/RetoForm/Estructura.js
+++ Projects/RetoForm/Estructura.js
@@ -9,43 +9,28 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (firstName.value === "") {
     addError(firstName, "first Name cannot be empty");
-    // Quité apropósito los `required` de los inputs para lograr esta funcionalidad que tenes acá,
-    // ya que si se tiene ese atributo entonces nunca te permitiría "CLAIM YOUR FREE TRIAL" con algún campos vacío
-
-    // Con estos mensajes que estás agregando (.innerText), te produce un bug,
-    // que cuando se envía con campos vacíos y el error aparece debajo de los inputs,
-    // al corregir los campos poniendo valores y volviendo a apretar el boton, el texto queda,
-    // Probalo vos mismos, te dejo una solución acá abajo:
-
-    // Estos mensajes los metas dentro del mismo small
-    // y que el addError y remove se conviertan en funciones para agregar o quitar una clase,
-    // por ejemplo 'show-error' o como te guste llamarla
-
-    // Al <small> dentro del cada le das un .Field: `display: none;` ó `visibility: hidden;`
-    // y a la clase podes darle un `display: block;` ó `visibility: visible;`
-
-    // y por ende, el comportamiento sería:
-    // -- si tiene la clase = entonces se vería darle el small por el estilo que la clase lleva
-    // -- si NO la tiene = entonces darle su `display: block;` proveniente de la clase, desaparece,
-    // lo que quita de la vista el mensaje de error
+    firstName.classList.add('errorIcon')
   } else {
     removeError(firstName);
   }
 
   if (lastName.value === "") {
     addError(lastName, "Last Name cannot be empty");
+    lastName.classList.add('errorIcon')
   } else {
     removeError(lastName);
   }
 
   if (email.value === "") {
     addError(email, "Looks like this is not an email");
+    email.classList.add('errorIcon')
   } else {
     removeError(email);
   }
 
   if (password.value === "") {
     addError(password, "Password cannot be empty");
+    password.classList.add('errorIcon')
   } else {
     removeError(password);
   }
@@ -57,6 +42,7 @@ function addError(input, message) {
   const small = input.parentNode.querySelector("small");
   small.innerText = message;
 }
+
 
 function removeError(input) {
   const FormControl = input.parentNode;
