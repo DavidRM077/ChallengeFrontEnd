b32100c10d44e299c27d91280716a18ffc005603
David Antonio Ramirez Mendez <davidrap087@gmail.com>
2024-04-05 00:23:47 -0400

    cambios realizados

---
+++ Projects/RetoForm/Estructura.js
@@ -0,0 +1,57 @@
+
+  const form = document.getElementById("FormActive");
+ 
+  
+form.addEventListener("submit", (e) => {
+ 
+  e.preventDefault();
+  
+  const firstName = form["imputOne"].value;
+  const secondName = form["lastname"].value;
+  const email = form["emailimput"].value;
+  const password = form["passwordi"].value;
+
+  if (firstName === "") {
+     
+    addError("imputOne", "first Name cannot be empty");
+  } else {
+    removeError("imputOne");
+  }
+
+  if (secondName === "") {
+    addError("lastname", "Last Name cannot be empty");
+  } else {
+    removeError("lastname");
+  }
+
+  if (email === "") {
+    addError("emailimput", "Looks like this is not an email");
+  } else {
+    removeError("emailimput");
+  }
+
+  if (password === "") {
+    addError("passwordi", "Password cannot be empty");
+  } else {
+    removeError("passwordi");
+  }
+   
+  });
+  
+  function addError(field, message) {
+   
+    const FormControl = form[field].parentNode;
+    FormControl.classList.add("errorMsg");
+    const small = form[field].parentNode.querySelector("small");
+    small.innerText = message;
+  }
+
+  
+  
+  function removeError(field) {
+    const FormControl = form[field].parentNode;
+    FormControl.classList.remove("errorMsg");
+   
+  }
+  
+  
